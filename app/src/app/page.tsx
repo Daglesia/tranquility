6cc5044cccf7ef15dd429ea1e25ab9d2793721c7
Magdalena <magdalena@daglesia.com>
2024-07-05 09:04:26 +0200

    (feat): Change landing page texts and font

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -19,14 +19,9 @@ export default function Home() {
         </div>
         <div className="pl-24 pt-64 ml-4">
           <div className="flex flex-col gap-2 w-fit">
-            <h1 className="text-h1 font-extrabold tracking-tighter">It’s time to stop the mundane.<br />
-              Let’s automate it together.</h1>
-            <h2 className="text-h2 font-normal">Monitor and automate daily chores.<br />
-              Completely <span className="bg-gradient-to-r from-primary via-secondary__variant-dark to-secondary inline-block text-transparent bg-clip-text animate-gradient bg-300%">free</span>.</h2>
-            <div className="w-fit self-center mt-14 flex flex-col gap-2">
-              <Button text={"Check it out!"} />
-              <p className="text-p text-secondary text-center font-extrabold">No registration needed!</p>
-            </div>
+            <h1 className="text-h1 font-extrabold tracking-tighter">Welcome to my domain.</h1>
+            <h2 className="text-h2 font-normal">Here’s everything to suit my fancy.<br />
+            Take a look, maybe <span className="bg-gradient-to-r from-primary via-secondary__variant-dark to-secondary inline-block text-transparent bg-clip-text animate-gradient bg-300%">yours</span> too.</h2>
           </div>
         </div>
       </div>
