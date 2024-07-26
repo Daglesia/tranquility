5e4c5995b8b1e568f59090ecb1ace900dc9c8fc2
Magdalena <magdalena@daglesia.com>
2024-07-26 15:46:43 +0200

    (feat): Introduce Vitest

--- app/src/components/CloudCluster.tsx
+++ app/src/components/CloudCluster.tsx
@@ -2,26 +2,10 @@
 
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Euler } from "three";
-import Cloud from "@/components/Cloud";
 
 function Scene() {
-  const clouds = [];
-  for (let p = 0; p < 50; p++) {
-    clouds.push(
-      <Cloud
-        key={p}
-        position={[Math.random() * 800, 500, Math.random() * 500 - 500]}
-        rotation={new Euler(1.16, -0.12, Math.random() * 2 * Math.PI)}
-      />,
-    );
-  }
-
   return (
     <>
-      <ambientLight color={"#83DEF0"} />
-      <fogExp2 color={"#83DEF0"} density={0.002} />
-      {clouds}
     </>
   );
 }
