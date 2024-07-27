788bc18808e56f1ace654d2850766ac3bc2bd664
Magdalena <magdalena@daglesia.com>
2024-07-27 21:00:55 +0200

    (feat): Fun with galaxy geometry

--- app/src/components/Cloud.tsx
+++ app/src/components/Cloud.tsx
@@ -10,16 +10,16 @@ export default function Cloud({ position }: CloudProps) {
     const [colorMap, alphaMap] = useLoader(TextureLoader, ["/texture.png",'/smoke.png']);
     const refMesh = useRef<Mesh<BufferGeometry, MeshLambertMaterial>>(null);
 
-    useFrame(() => {
-        if (refMesh.current) {
-            refMesh.current.rotation.z += 0.0004;
-        }
-    })
+    // useFrame(() => {
+    //     if (refMesh.current) {
+    //         refMesh.current.rotation.z += 0.0004;
+    //     }
+    // })
 
     return <>
         <mesh ref={refMesh} position={position} rotation={[-0.2,0,0]}>
             <planeGeometry args={[10, 10]} />
-            <meshBasicMaterial alphaMap={alphaMap} map={colorMap} transparent={true} opacity={1} />
+            <meshBasicMaterial alphaMap={alphaMap} color="red" transparent={true} opacity={0.3} />
         </mesh>
     </>
 }
--- app/src/components/CloudCluster.tsx
+++ app/src/components/CloudCluster.tsx
@@ -4,6 +4,8 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Euler } from 'three'
 import Cloud from '@/components/Cloud'
+import { Particles } from './Particles'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
 function Scene() {
   const clouds = []
@@ -22,7 +24,12 @@ export default function Nebula() {
       position: [0, 0, 10],
     }}>
       <Suspense fallback={null}>
-        <Scene />
+        {/* <Cloud position={[5,0,0]} /> */}
+        <Particles/>
+        <EffectComposer multisampling={8}>
+            <Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={0.4} intensity={0.6} />
+            
+        </EffectComposer>
       </Suspense>
     </Canvas>
   )
